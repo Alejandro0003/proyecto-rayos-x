6bf288ff7678b3d110016d8f482738e558d4e16e
Alejandro0003 <144927543+Alejandro0003@users.noreply.github.com>
2026-05-28 21:56:48 -0500

    Update app.py

--- proyecto_rayos_x/app.py
+++ proyecto_rayos_x/app.py
@@ -52,8 +52,11 @@ st.title("Control de Seguridad Aeroportuaria- Inspección por Rayos X")
 st.markdown("---")
 
 # Fila Superior: Tarjetas de Métricas de Rendimiento del Modelo de IA
-col_m4 = st.columns(4)
-
+col_m1, col_m2, col_m3, col_m4 = st.columns(4)
+with col_m2:
+    st.metric(label="Umbral de Confianza", value="70%")
+with col_m3:
+    st.metric(label="Tiempo de Inferencia", value="142 ms")
 with col_m4:
     st.metric(label="Estado del Sistema", value="Operativo")
 
